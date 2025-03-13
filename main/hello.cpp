d53a391a38f230495662951572239fee7d6426c4
Safwan Ahmed <safwanahmed2004@gmail.com>
2025-03-13 16:28:31 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this\n";
   return 0;
 }
